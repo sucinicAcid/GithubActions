9fa54e9fa76d62e5b06f5c4fa02bb6069474b91d
석진성 <syhan655@naver.com>
2025-05-11 18:33:28 +0900

    테스트가 성공하지 못하도록 수정

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 def bubble_sort(arr):
     # n은 arr의 길이
-    n = len(arr)
-    for i in range(n):
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+    # n = len(arr)
+    # for i in range(n):
+    #     for j in range(0, n-i-1):
+    #         if arr[j] > arr[j+1]:
+    #             arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
